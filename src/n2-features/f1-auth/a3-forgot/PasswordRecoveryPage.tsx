07b3a67df859fce9b8a79b4798fc200e927db4cb
DinaSem <neispravimayas1@gmail.com>
2022-03-31 10:45:18 +0300

    recoveryPage

--- src/n2-features/f1-auth/a3-forgot/PasswordRecoveryPage.tsx
+++ src/n2-features/f1-auth/a3-forgot/PasswordRecoveryPage.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import {NavLink} from "react-router-dom";
+import {PATH} from "../../../n1-main/m1-ui/routes/Paths";
+
 
 const PasswordRecoveryPage = () => {
+
+    const[email, setEmail]= useState('')
+    const emailHandler = (e:ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+        const emailOnClickHandler = () => {
+        }
+
     return (
         <div>
-            <h3>Password Recovery Page</h3>
+            <div>
+                <input type={'text'}
+                       value={email}
+                       placeholder={'email'}
+                       onChange={emailHandler}
+                />
+            </div>
+            <button onClick={emailOnClickHandler}>sign up</button>
+            <div><NavLink to={PATH.LOGIN}>login</NavLink></div>
         </div>
     );
 };
